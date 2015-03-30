f38bfa1d22048a3a49936aee1b6b08d2f00c031f
Vinh Peters <peters.685@osu.edu>
2015-03-30 19:04:17 -0400

    Updated to nav functions from PT3
    Also made it a header file

--- nav.h
+++ nav.h
@@ -30,8 +30,7 @@ void move_time(int Rpercent, int Lpercent, float sec)
     left_motor.SetPercent(Lpercent);
     //uses time library
     float time = TimeNow();
-    while(TimeNow()-time.sec);
-
+    while(TimeNow()-time<sec);
     //stops after time has passed
     right_motor.Stop();
     left_motor.Stop();
@@ -49,7 +48,7 @@ void move_forward(int percent, int counts) //using encoders
 
     //While the average of the left and right encoder are less than counts,
     //keep running motors
-    while((left_encoder.Counts() + right_encoder.Counts()) / 2counts);
+    while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);
 
     //Turn off motors
     right_motor.Stop();
@@ -71,10 +70,9 @@ void turn_right(int percent, int counts) //using encoders
     //While the average of the left and right encoder are less than counts,
     //keep running motors
 
-    while(((left_encoder.Counts() + right_encoder.Counts()) / 2.) <=counts);
+    while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);
 
     //Turn off motors
-
     right_motor.Stop();
     left_motor.Stop();
 }
@@ -94,7 +92,7 @@ void turn_left(int percent, int counts) //using encoders
     //While the average of the left and right encoder are less than counts,
     //keep running motors
 
-    while(((left_encoder.Counts() + right_encoder.Counts()) / 2.)<=counts);
+    while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);
 
     //Turn off motors
     right_motor.Stop();
@@ -104,17 +102,17 @@ void turn_left(int percent, int counts) //using encoders
 void check_x_plus(float x_coordinate) //using RPS while robot is in the +x direction
 {
     //check whether the robot is within an acceptable range
-    while(RPS.X() " x_coordinate - 2 || RPS.X() " x_coordinate + 2)
+    while(RPS.X() < x_coordinate - 2 || RPS.X() > x_coordinate + 2)
     {
         //If RPS coordinate is greater than the target coordinate,
         //you have gone too far and must pulse backward
-        if(RPS.X() " x_coordinate)
+        if(RPS.X() > x_coordinate)
         {
             right_motor.SetPercent(-30);
             left_motor.SetPercent(-30);
         }
         //in this case you have not gone far enough and must pulse forward
-        else if(RPS.X() " x_coordinate)
+        else if(RPS.X() < x_coordinate)
         {
             right_motor.SetPercent(30);
             left_motor.SetPercent(30);
@@ -127,16 +125,16 @@ void check_x_plus(float x_coordinate) //using RPS while robot is in the +x direc
 void check_y_minus(float y_coordinate) //using RPS while robot is in the -y direction
 {
     //check whether the robot is within an acceptable range
-    while(RPS.Y() " y_coordinate - 2 || RPS.Y() " y_coordinate + 2)
+    while(RPS.Y() < y_coordinate - 2 || RPS.Y() > y_coordinate + 2)
     {
         //You have not gone far enough, must pulse forward
-        if(RPS.Y() " y_coordinate)
+        if(RPS.Y() > y_coordinate)
         {
             right_motor.SetPercent(30);
             left_motor.SetPercent(30);
         }
         //You have gone too far, must pulse backward
-        else if(RPS.Y() " y_coordinate)
+        else if(RPS.Y() < y_coordinate)
         {
             right_motor.SetPercent(-30);
             left_motor.SetPercent(-30);
@@ -149,16 +147,16 @@ void check_y_minus(float y_coordinate) //using RPS while robot is in the -y dire
 void check_y_plus(float y_coordinate) //using RPS while robot is in the +y direction
 {
     //check whether the robot is within an acceptable range
-    while(RPS.Y() " y_coordinate - 2 || RPS.Y() " y_coordinate + 2)
+    while(RPS.Y() < y_coordinate - 2 || RPS.Y() > y_coordinate + 2)
     {
         //You have gone too far, must pulse backward
-        if(RPS.Y() " y_coordinate)
+        if(RPS.Y() > y_coordinate)
         {
             right_motor.SetPercent(-30);
             left_motor.SetPercent(-30);
         }
         //You have not gone far enough, must pulse forward
-        else if(RPS.Y() " y_coordinate)
+        else if(RPS.Y() < y_coordinate)
         {
             right_motor.SetPercent(30);
             left_motor.SetPercent(30);
@@ -170,15 +168,15 @@ void check_y_plus(float y_coordinate) //using RPS while robot is in the +y direc
 
 void check_heading(float heading) //using RPS
 {
-    while(RPS.Heading() " heading - 2 || RPS.Heading() " heading + 2)
+    while(RPS.Heading() < heading - 2 || RPS.Heading() > heading + 2)
     {
         //If RPS heading is greater than target heading
-        if(RPS.Heading() " heading)
+        if(RPS.Heading() > heading)
         {
             // Ex. at 357 and trying to get to 0, turn left
             //If the difference is greater than 180, then the robot
             //should be turning left to get to target heading
-            if (RPS.Heading()-heading"180)
+            if (RPS.Heading()-heading>180)
             {
                 //turn left 1 count
                 turn_left(40,1);
@@ -195,11 +193,11 @@ void check_heading(float heading) //using RPS
             }
         }
         //If RPS heading is less than target heading
-        else if(RPS.Heading()<heading)
+        else if(RPS.Heading() < heading)
         {
             // Ex. At 0 and trying to get to 357, turn right
             //if difference is greater than 180, turn right
-            if (heading-RPS.Heading()"180)
+            if (heading-RPS.Heading()>180)
             {
                 //turn right 1 count
                 turn_right(40,1);
@@ -217,8 +215,29 @@ void check_heading(float heading) //using RPS
     }
 }
 
-void SetHeading(float heading)
+//function when you want to turn relative to RPS location
+//takes desired new location coordinates as parameters
+//should turn robot so that it is facing this location
+float turnRPS(float xc, float yc)
 {
+    //stores current location of robot
+    float x=RPS.X();
+    float y=RPS.Y();
+    //stores change in x and y
+    float dx=xc-x;
+    float dy=yc-y;
+    //calculates heading (Note: must be converted from radians to degrees)
+    float heading=atan(dy/dx)*(180/PI);
+    //accounts for 0 degrees being north instead of east
+    if (dx>0)
+    { //add 270 to rotate one quadrant CCW
+        heading+=270;
+    }
+    else
+    { //range of atan is only (-PI/2, PI/2), so if you want to be in
+        //quadrants 2 or 3 you add 180 then 270, or just add 90
+        heading+=90;
+    }
     //stores actual rotational distance you want to travel
     float dist;
     //counts to travel that distance
@@ -226,10 +245,10 @@ void SetHeading(float heading)
     //Stores current RPS heading
     //(RPS heading fluctuates so it's easier to store it in a variable at the beginning)
     float Rhead=RPS.Heading();
-    if (heading-Rhead"0)
+    if (heading-Rhead>0)
     {
         //it would be faster to turn right
-        if (heading-Rhead"180)
+        if (heading-Rhead>180)
         {
             dist=(360-heading)+Rhead;
             //calculates counts based on the ratio of 11 counts per 90 degrees
@@ -247,7 +266,7 @@ void SetHeading(float heading)
     else
     {
         //it would be faster to turn left
-        if(Rhead-heading"180)
+        if(Rhead-heading>180)
         {
             dist=(360-Rhead)+heading;
             counts=dist*(11.0/90);
@@ -262,86 +281,7 @@ void SetHeading(float heading)
         }
     }
     //fine-tuning
-
     check_heading(heading);
-}
-
-//moves robot based on an equation or specified path to an objective
-void move_f(float x, float y, float head, int percent)
-{
-    //finds angle relative to horizontal
-    if(head"180)
-    {
-        head=head-270;
-    }
-    else
-    {
-        head=head-90;
-    }
-    //calculates equation of path to objective (y=mx+b)
-    float m = tan(head);
-    float b = y/(x*m);
-    //store current location and orientation of robot
-    float head_now=RPS.Heading();
-    float xc=RPS.X();
-    float yc=RPS.Y();
-    //you are below the path
-    //finds angle to travel perpendicularly to path, shortest distance
-    float head_later=atan((-1)/m);
-    //corrections relavive to RPS system
-    //for negative slopes
-    if(head_later"0)
-    {
-        head_later=360-head_later;
-    }
-    //for positive slopes
-    else
-    {
-        head_later+=270;
-    }
-    if(yc"(m*RPS.X()+b))
-    {
-        head_later-=180;
-    }
-    SetHeading(head_later);
-    right_motor.SetPercent(50);
-    left_motor.SetPercent(50);
-    if(yc"(m*RPS.X()+b))
-    {
-        while(RPS.Y()"(m*RPS.X()+b));
-    }
-    else
-    {
-        while(RPS.Y()"(m*RPS.X()+b));
-    }
-    right_motor.Stop();
-    left_motor.Stop();
-}
-
-//function when you want to turn relative to RPS location
-//takes desired new location coordinates as parameters
-//and returns the heading towards that location
-
-float turnRPS(float xc, float yc)
-{
-    //stores current location of robot
-    float x=RPS.X();
-    float y=RPS.Y();
-    //stores change in x and y
-    float dx=xc-x;
-    float dy=yc-y;
-    //calculates heading (Note: must be converted from radians to degrees)
-    float heading=atan(dy/dx)*(180/PI);
-    //accounts for 0 degrees being north instead of east
-    if (dx"0)
-    { //add 270 to rotate one quadrant CCW
-        heading+=270;
-    }
-    else
-    { //range of atan is only (-PI/2, PI/2), so if you want to be in
-        //quadrants 2 or 3 you add 180 then 270, or just add 90
-        heading+=90;
-    }
     return heading;
 }
 #endif
